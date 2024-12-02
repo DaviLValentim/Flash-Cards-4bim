905d27fb6db0e1558d398d45177b1e21e774321b
Davi Valentim <136862737+DaviLValentim@users.noreply.github.com>
2024-12-01 21:02:13 -0300

    Arquivos Site

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Física - Inércia',
+    'Qual lei de Newton é conhecida como a Lei da Inércia?',
+    ' A Primeira Lei de Newton.'
+)
+
+criaCartao(
+    'Física - Inércia',
+    'O que acontece com um corpo em movimento sem forças externas?',
+    'Continua em movimento retilíneo uniforme.'
+)
+
+criaCartao(
+    'Física - Inércia',
+    'O que acontece com um corpo em repouso sem forças externas?',
+    'Permanece em repouso.'
+)
+
+criaCartao(
+    'Física - Inércia',
+    'Qual grandeza física mede a resistência de um corpo à mudança de movimento?',
+    'Massa.'
+)
